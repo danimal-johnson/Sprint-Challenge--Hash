aa726a70981708ca402fcfea0ef215aa06546766
Dan Johnson <danimal.johnson@gmail.com>
2020-05-08 08:51:15 -0700

    Exercise 1: weight indices

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
-
     """
     YOUR CODE HERE
     """
+    ht = {}  # Using a dictionary as a hash table.
+
+    # Fill
+    for i in range(length):
+        ht[weights[i]] = i  # { curr_weight: index }
+
+    for i in range(length):
+        weight_complement = limit - weights[i]
+        if weight_complement in ht:
+            return_val = [i, ht[weight_complement]]
+            return_val.sort(reverse=True)
+            return return_val
 
     return None
